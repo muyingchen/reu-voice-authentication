25876333a5656db994b0f35ab2bddd9f432ee970
muyingchen <muyingchen@berkeley.edu>
2017-07-15 16:52:25 -0400

    fixed recordAudio.py

--- python_scripts/recordAudio.py
+++ python_scripts/recordAudio.py
@@ -3,44 +3,41 @@ import wave
 import sys
 import os
 
-class speechRecognition:
-    def __init__(self):
+if len(sys.argv) < 2:
+    print("Please input a name for output audio")
+    sys.exit(-1)
 
-    # if len(sys.argv) < 2:
-    #     print("Please input a name for output audio")
-    #     sys.exit(-1)
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+RECORD_SECONDS = 5
+WAVE_OUTPUT_FILENAME = sys.argv[1] + ".wav"
+p = pyaudio.PyAudio()
 
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = sys.argv[1] + ".wav"
-    p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
+print("* recording")
 
-    print("* recording")
+frames = []
 
-    frames = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
+print("* done recording")
 
-    print("* done recording")
+stream.stop_stream()
+stream.close()
+p.terminate()
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames))
+wf.close()
