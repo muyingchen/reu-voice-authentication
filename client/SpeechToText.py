26710265ca734f62feba7417239d51d17839e390
muyingchen <muyingchen@berkeley.edu>
2017-07-22 14:43:47 -0400

    fixed comments for stt

--- client/SpeechToText.py
+++ client/SpeechToText.py
@@ -3,8 +3,10 @@ from os.path import join, dirname, abspath
 import config
 import json
 
+# Extract text from speech audio file using watson speech to text API
 class SpeechToText:
     def __init__(self):
+        # load API key information from config
         self.config = config.speech_to_text_config ;
         if not self.config['username']:
             raise ValueError('No IBM Watson credential username found.')
@@ -16,8 +18,9 @@ class SpeechToText:
             password = self.config['password'],
             x_watson_learning_opt_out=False)
 
+    # get text result for a recognized speech
     def get_text(self, audio_file=None):
-        # get a text result for recognized speech
+        # if no audio file is passed to the function, use default audio
         if not audio_file:
             audio_file = open(join(dirname(abspath(__file__)), 'data/wave/test4.wav'), 'rb')
         try:
@@ -37,18 +40,19 @@ class SpeechToText:
             print(err)
             pass
 
+    # retrieve transcript from the json object represented text
     def get_transcript(self, text):
-        # retrieve transcript from the json object represented text
         return text['results'][0]['alternatives'][0]['transcript']
 
+    # return a list of language models that Watson speech-to-text engine support
     def get_language_model(self):
-        # return a list of language models that Watson speech-to-text engine support
         m = self.speech_to_text.models()
         models = []
         for i in m['models']:
             models.append(i['name'])
         return models
 
+    # write the recognized text into a txt file
     def write_text(self, text):
         with open('output.txt', 'w') as text_file:
             text_file.write(text + '\n')
