c763e5bb22863ab88c031176914caf111412463a
muyingchen <muyingchen@berkeley.edu>
2017-08-01 12:12:38 -0400

    added config.py

---
+++ client/config.py
@@ -0,0 +1,11 @@
+speech_to_text_config = {
+  'url': 'https://stream.watsonplatform.net/speech-to-text/api',
+  'username': '...',
+  'password': '...'
+}
+
+voiceit_config = {
+    'developerId' : '...',
+    'developerUserId' : '...',
+    'password' : '..'.encode('utf-8')
+}
